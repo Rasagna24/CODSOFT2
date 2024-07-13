d16ba5dbfc808e2dfce10b6697afaec6689530ae
Rasagna24 <150986714+Rasagna24@users.noreply.github.com>
2024-07-13 15:02:10 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,4 @@
+// JavaScript code can be added here if needed for interactive features
+function toggleProjectDetails(project) {
+    project.querySelector('.project-details').classList.toggle('show');
+}
